5db77690b6e389ed23870c0c06c9c9fef92cf9b8
PANGJUN <https://github.com/Hwang-jun/Tmap_example_java.git>
2022-10-21 22:27:07 +0900

    수정

--- app/src/main/java/com/example/tmap_example/MainActivity.java
+++ app/src/main/java/com/example/tmap_example/MainActivity.java
@@ -2,69 +2,90 @@ package com.example.tmap_example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.skt.Tmap.TMapGpsManager;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 import com.skt.Tmap.poi_item.TMapPOIItem;
 
-import android.content.res.Resources;
+import android.Manifest;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PointF;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
 
     String API_KEY = "l7xx6c11a7128c354fa4a9944f2cab668afd"; // API_KEY
+    TMapView tMapView = null; // T map View
+    TMapGpsManager tMapGPS = null; // GPS 사용
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TMapView tMapView = new TMapView(this); // T map View
 
         // API_KEY
         tMapView.setSKTMapApiKey(API_KEY);
+        // View, GPS
+        tMapView = new TMapView(this);
+        tMapGPS = new TMapGpsManager(this);
 
         // Initial setting
         tMapView.setZoomLevel(17);
         tMapView.setIconVisibility(true);
         tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
-        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN); // 한국어 사용
+
+        tMapGPS.setMinTime(1000);
+        tMapGPS.setMinDistance(10);
+        tMapGPS.setProvider(tMapGPS.NETWORK_PROVIDER);
+        tMapGPS.OpenGps();
 
         // T Map 보는데 사용되는 레이아웃
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayoutTmap);
         linearLayout.addView(tMapView);
 
-        // 클릭 이벤트 설정
-        tMapView.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback() {
-            @Override
-            public boolean onPressEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
-                return false;
-            }
 
-            @Override
-            public boolean onPressUpEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
-                return false;
-            }
-        });
+        // gps 허가 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+//        // 클릭 이벤트 설정
+//        tMapView.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback() {
+//            @Override
+//            public boolean onPressEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onPressUpEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+//                return false;
+//            }
+//        });
 
         // 마커 생성
         TMapMarkerItem markerItem1 = new TMapMarkerItem();
-        TMapPoint tMapPoint1 = new TMapPoint(35.1208, 129.1012); // 동명대 경도 위도
+        //TMapPoint tMapPoint1 = new TMapPoint(35.1208, 129.1012); // 동명대 경도 위도
+        // TMapPoint tpoint = tMapView.getLocationPoint();
+        // double Latitude = tpoint.getLatitude();
+        // double Longitude = tpoint.getLongitude();
 
         // 마커 아이콘
         Bitmap bitmap = BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.map_pin_red);
 
         markerItem1.setIcon(bitmap);
-        markerItem1.setPosition(0.5f, 1.0f);
-        markerItem1.setTMapPoint(tMapPoint1);
-        markerItem1.setName("동명대학교");
-        tMapView.addMarkerItem("markerItem1", markerItem1);
-        tMapView.setCenterPoint(129.1012, 35.1208);
+    }
+
+    @Override
+    public void onLocationChange(Location location) {
+        tMapView.setLocationPoint(location.getLongitude(), location.getLatitude());
+        tMapView.setCenterPoint(location.getLongitude(), location.getLatitude());
     }
 }
